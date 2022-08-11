2bc6226a41cc447af371f338afbfa0a23b722c68
waryu-YND <83166243+waryu-YND@users.noreply.github.com>
2022-08-11 22:20:37 +0900

    Fix img path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
-console.log("urlPrefix::: " + urlPrefix);
 
 module.exports = {
   assetPrefix: urlPrefix,
   basePath: urlPrefix,
   trailingSlash: true,
+  publicRuntimeConfig: { urlPrefix }
 };
