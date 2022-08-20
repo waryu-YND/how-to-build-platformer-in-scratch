29341ba78deb39a355c4d7eadd8ffc6a7945f903
waryu-YND <waryu-YND@users.noreply.github.com>
2022-08-20 16:21:29 +0000

    deploy: 4de7be8ac301f4e3304b183fbf4f09337de203fa

---
+++ _next/static/EOd__hb3pITK01eRKgkIW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-bc1dd906a3782f70.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/docs/[slug]":["static/chunks/pages/docs/[slug]-dc4ec7e3329e7732.js"],sortedPages:["/","/_app","/_error","/docs/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
